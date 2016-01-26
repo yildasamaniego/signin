a8bb9b8df037e85e9aeb0c231a520f04319cf0d8
Graphic <graphic@SOD-SJH-605-17M.local>
2016-01-26 18:00:17 -0500

    class

---
+++ js/main.js
@@ -0,0 +1,16 @@
+
+
+$('.signin').on('click',function(){
+$('.modal').fadeIn(900);
+       
+ });
+
+$('.close').on('click',function(){
+$('.modal').fadeOut(900);
+       
+ });
+
+$('.submit').on('click',function(){
+$.css("error").fadeIn();
+       
+ });
